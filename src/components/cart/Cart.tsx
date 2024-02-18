fa05771ab16e26edb131bb17a5f0a3f05d698a90
wunlei <confusioncosmos@gmail.com>
2024-02-18 16:50:45 +0300

    feat: add cart animation

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "src/store/hooks";
 import { cartSelector } from "src/store/selectors/selectors";
 import { updateCartFromLS } from "src/store/slices/appSlice";
@@ -15,6 +15,7 @@ type CartProps = {
 function Cart(props: CartProps) {
   const dispatch = useAppDispatch();
   const cartItems = useAppSelector(cartSelector);
+  const [isClosing, setIsClosing] = useState(false);
 
   let positionsCount = 0;
   let sum = 0;
@@ -23,6 +24,13 @@ function Cart(props: CartProps) {
     positionsCount += el.count;
     sum += el.count * el.price;
   });
+
+  function beforeClose() {
+    setIsClosing(true);
+    setTimeout(() => {
+      props.setIsOpen(false);
+    }, 200);
+  }
 
   useEffect(() => {
     function handleLSTabUpdate(e: StorageEvent) {
@@ -40,54 +48,64 @@ function Cart(props: CartProps) {
   }, [dispatch]);
 
   return (
-    <div
-      className={`${classes["cart"]} ${props.isOpen ? classes["cart-open"] : ""}`}
-    >
-      <div className={classes["cart-header"]}>
-        <div className={`${classes["btn-cart"]} text-m`}>
-          <CartIcon />
-          cart
-        </div>
-        <button
-          className={classes[`btn-cart-close`]}
-          onClick={() => {
-            props.setIsOpen(false);
-          }}
-        >
-          <CrossIcon />
-        </button>
-      </div>
-      <div className={classes["cart-items"]}>
-        {cartItems.length === 0 ? (
-          <div>Cart is empty</div>
-        ) : (
-          cartItems.map((el) => <CartItem key={el.id} {...el} />)
-        )}
-      </div>
-      <div className={classes["cart-footer"]}>
-        {cartItems.length === 0 ? (
+    <>
+      <div
+        className={`${classes["overlay"]} ${props.isOpen && classes["overlay-open"]} ${isClosing && classes["overlay-closing"]}`}
+        onClick={() => {
+          beforeClose();
+        }}
+      ></div>
+      <div
+        className={`${classes["cart"]} ${props.isOpen && classes["cart-open"]} ${isClosing && classes["cart-closing"]}`}
+      >
+        <div className={classes["cart-header"]}>
+          <div className={`${classes["btn-cart"]} text-m`}>
+            <CartIcon />
+            cart
+          </div>
           <button
-            className={`btn ${classes["btn-cart_big"]} text-l`}
+            className={classes[`btn-cart-close`]}
             onClick={() => {
-              props.setIsOpen(false);
+              beforeClose();
             }}
           >
-            back to products
+            <CrossIcon />
           </button>
-        ) : (
-          <>
-            <div className={`${classes["cart-total-count"]} text-xs`}>
-              {positionsCount} positions
-            </div>
-            <div className={"cart-total-sum text-xl"}>${sum}</div>
-
-            <button className={`btn ${classes["btn-cart_big"]} text-l`}>
-              place an order
+        </div>
+        <div className={classes["cart-items"]}>
+          {cartItems.length === 0 ? (
+            <div>Cart is empty</div>
+          ) : (
+            cartItems.map((el) => <CartItem key={el.id} {...el} />)
+          )}
+        </div>
+        <div className={classes["cart-footer"]}>
+          {cartItems.length === 0 ? (
+            <button
+              className={`btn btn_accent text-l ${classes["btn-cart_footer"]} text-l`}
+              onClick={() => {
+                beforeClose();
+              }}
+            >
+              back to products
             </button>
-          </>
-        )}
+          ) : (
+            <>
+              <div className={`${classes["cart-total-count"]} text-xs`}>
+                {positionsCount} positions
+              </div>
+              <div className={"cart-total-sum text-xl"}>${sum}</div>
+
+              <button
+                className={`btn btn_accent text-l ${classes["btn-cart_footer"]} text-l`}
+              >
+                place an order
+              </button>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
